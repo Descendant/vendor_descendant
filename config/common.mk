fc0aeb1e302ef8dfd289b264f40d25dc3bef2c68
Dil3mm4 <dil3mm4.dev@gmail.com>
2018-08-20 18:08:39 +0000

    Initial commit
    build tools;
    some prebuilts;
    bootanimation;
    default wallpaper;
    rounded searchbar settings overlay;

---
+++ config/common.mk
@@ -0,0 +1,39 @@
+# Copyright (C) 2018 Descendant
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#Device Codename
+
+DESCENDANT_DEVICE := $(subst descendant_,,$(TARGET_PRODUCT))
+
+#Build Type
+
+ifndef BUILDTYPE
+    BUILDTYPE := UNOFFICIAL
+else
+    BUILDTYPE := OFFICIAL
+endif
+
+#Versioning
+
+MAJOR_VER := One
+MINOR_VER := Zero
+DESCENDANT_NAME := Descendant-$(MAJOR_VER)dot$(MINOR_VER)_$(BUILDTYPE)-$(DESCENDANT_DEVICE)
+
+#Overlays
+
+DEVICE_PACKAGE_OVERLAYS += vendor/descendant/overlay/common
+
+#Prebuilts
+include vendor/descendant/prebuilts/pre.mk
---
+++ prebuilt/apps/BraveTechnology/BraveBrowser/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := bravebrowser
+LOCAL_MODULE_OWNER := bravetechnology
+LOCAL_SRC_FILES := bravebrowser.apk
+LOCAL_CERTIFICATE := shared
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_DEX_PREOPT := false
+LOCAL_OVERRIDES_PACKAGES := Browser Browser2
+include $(BUILD_PREBUILT)
