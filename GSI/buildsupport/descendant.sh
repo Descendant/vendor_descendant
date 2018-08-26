6500b10b4dab7ce51a53d81fe5ec30c4c855b0aa
Dil3mm4 <dil3mm4.dev@gmail.com>
2018-08-26 20:59:25 +0000

    Add more (and missing stuff) for GSI build support

--- GSI/buildsupport/descendant.sh
+++ GSI/buildsupport/descendant.sh
@@ -1,12 +1,13 @@
-bash "$(dirname "$0")/apply-patches.sh" patches
+(cd patches && git pull phh android-9.0)
+bash "apply-patch.sh" patches
 
 . build/envsetup.sh
 
 buildVariant() {
-	lunch treble_arm64_avN.mk
+	lunch treble_arm64_avN
 	make WITHOUT_CHECK_API=true BUILD_NUMBER=$rom_fp installclean
 	make WITHOUT_CHECK_API=true BUILD_NUMBER=$rom_fp -j$jobs systemimage
 	make WITHOUT_CHECK_API=true BUILD_NUMBER=$rom_fp vndk-test-sepolicy
 }
 
-buildVariant
+buildVariant treble_arm64_avN-userdebug
