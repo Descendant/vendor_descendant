0066c68a89ca3cbbf2ca49ca5ea487589b4f4384
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-09-10 16:22:25 +0000

    Add Google Sans family

--- config/common.mk
+++ config/common.mk
@@ -52,3 +52,12 @@ include vendor/descendant/prebuilt/pre.mk
 #Include Changelog
 PRODUCT_COPY_FILES += \
 	vendor/descendant/GSI/Changelog.txt:system/etc/Changelog.txt
+
+#Fonts
+PRODUCT_COPY_FILES += \
+    vendor/descendant/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+    vendor/descendant/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+    vendor/descendant/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+    vendor/descendant/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+    vendor/descendant/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+    vendor/descendant/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
