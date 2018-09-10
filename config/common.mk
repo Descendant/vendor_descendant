b56e65b00b2334fe81124f49b8658a4cf4dac1f9
Mazda-- <alex@dirtyunicorns.com>
2018-09-10 23:13:43 +0000

    Fruity pebbles [2/4]

--- config/common.mk
+++ config/common.mk
@@ -66,3 +66,26 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
      vendor/descendant/prebuilts/configs/pixel.xml:system/etc/sysconfig/pixel.xml
 
+#Accents
+PRODUCT_PACKAGES += \
+    AmberAccent \
+    BlackAccent \
+    BlueAccent \
+    BlueGreyAccent \
+    BrownAccent \
+    CyanAccent \
+    DeepOrangeAccent \
+    DeepPurpleAccent \
+    GreenAccent \
+    GreyAccent \
+    IndigoAccent \
+    LightBlueAccent \
+    LightGreenAccent \
+    LimeAccent \
+    OrangeAccent \
+    PinkAccent \
+    PurpleAccent \
+    RedAccent \
+    TealAccent \
+    YellowAccent \
+    WhiteAccent
