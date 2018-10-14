019869c487e1cf4e0c6c2340683632c962bbe8df
darkestDark <36689074+darkestDark@users.noreply.github.com>
2018-10-14 16:42:48 +0200

    Create android.mk

---
+++ prebuilt/apps/SimpleMobileTools/Simple-Calculator/android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := Simple-Calculator
+LOCAL_MODULE_OWNER := SimpleMobileTools
+LOCAL_SRC_FILES := Simple-Calculator.apk
+LOCAL_CERTIFICATE := shared
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_DEX_PREOPT := false
+LOCAL_OVERRIDES_PACKAGES := calculator
+include $(BUILD_PREBUILT)
