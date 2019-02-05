4d9071caf8194716da917a7875d7199b56ed9218
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-02-05 18:23:48 +0000

    Remove un-used prebuilts

--- prebuilt/pre.mk
+++ prebuilt/pre.mk
@@ -20,7 +20,6 @@ PRODUCT_PACKAGES += \
         CameraRoll \
         Markup \
         RetroMusicPlayer \
-        CalculatorPre \
         OpenCamera \
         SimpleCalculator \
         AmazeFileManager
