15df605826a2c109cd5337302b0dfdbf31ce42e1
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-02-26 15:45:18 +0000

    Copyright year update

--- prebuilt/pre.mk
+++ prebuilt/pre.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 Descendant
+# Copyright (C) 2019 Descendant
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
