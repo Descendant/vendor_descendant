e055dd6c6cf0ab3b3b5993663f950b9877511321
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-03-08 15:12:24 +0000

    Version bump

--- config/common.mk
+++ config/common.mk
@@ -28,7 +28,7 @@ DESCENDANT_DEVICE := $(subst descendant_,,$(TARGET_PRODUCT))
 #Versioning
 
 MAJOR_VER := Two
-MINOR_VER := Zero
+MINOR_VER := One
 DESCENDANT_NAME := $(MAJOR_VER)Dot$(MINOR_VER)
 
 #Custom Properties
