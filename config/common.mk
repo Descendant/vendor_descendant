AOSDP/android_vendor_aosdp

vendor: Backuptool script update
Bring inline with LineageOS backup script

Change-Id: If139dda6a52ac1d0f0f421c54873e127ad070899

config/common.mk
@@ -13,6 +13,7 @@ PRODUCT_COPY_FILES += \
     vendor/aosdp/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
     vendor/aosdp/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
     vendor/aosdp/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh \
+    vendor/aosdp/prebuilt/common/bin/blacklist:system/addon.d/blacklist
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     keyguard.no_require_sim=true \

Descendant/vendor_descendant

config/common.mk
@@ -80,6 +80,7 @@ PRODUCT_COPY_FILES += \
     vendor/descendant/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
     vendor/descendant/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
     vendor/descendant/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh \
+    vendor/descendant/prebuilt/common/bin/blacklist:system/addon.d/blacklist
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     keyguard.no_require_sim=true \