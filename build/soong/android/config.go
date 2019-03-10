73b03e6b7b32411b2c6351c4cb324e410eeb1213
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-03-10 01:47:49 +0000

    Rename

--- build/soong/android/config.go
+++ build/soong/android/config.go
@@ -1,7 +1,7 @@
 package android
 
 // Global config used by Aosdp soong additions
-var AosdpConfig = struct {
+var DescendantConfig = struct {
 	// List of packages that are permitted
 	// for java source overlays.
 	JavaSourceOverlayModuleWhitelist []string
